82f5f85affb440821e60bd5442fd8d056bbdcc65
rrelyk <kystik94@gmail.com>
2025-03-25 10:32:47 -0400

    CH2 - L2: TextNode Tests

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2, "TextNodes are equal.")
+    def test_ne(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2, "TextNodes are equal.")
+    def test_text_type_ne(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2, "TextNodes are NOT equal.")
+    
+
+
+
+if __name__ == "__main__":
+    unittest.main()
