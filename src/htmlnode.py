edf95cde9ed325e7aaa9cca788dae7ed8aae08c2
rrelyk <kystik94@gmail.com>
2025-03-31 11:06:43 -0400

    CH2 - L4: LeafNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,4 +24,38 @@ class HTMLNode:
         return "".join(f' {key}="{value}"' for key, value in self.props.items())
     
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode(tag='{self.tag}', value='{self.value}', children='{self.children}', props='{self.props}')"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value
+        
+        # List of self-closing tags
+        self_closing_tags = ["img", "input", "br", "hr", "meta", "link"]
+
+        #open front tag
+        front_tag = "<" + self.tag
+        #add props if existing
+        if self.props:
+            for key, value in self.props.items():
+                front_tag += f' {key}="{value}"'
+    
+        # Handle self-closing tags differently
+        if self.tag.lower() in self_closing_tags:
+            front_tag += " />"
+            return front_tag
+        else:
+            # Close front tag normally for regular tags
+            front_tag += ">"
+            back_tag = "</" + self.tag + ">"
+            return front_tag + self.value + back_tag
+    
+
+    
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,6 +35,34 @@ class TestHTMLNode(unittest.TestCase):
     def test_empty_props_dict(self):
         node = HTMLNode(props={})
         self.assertEqual(node.props_to_html(), "", "Empty props dict should return empty string")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>", "LeafNode 'p' tag should be rendered correctly")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>', "LeafNode 'a' tag should be rendered correctly")
+    
+    def test_leaf_to_html_img(self):
+        node = LeafNode("img", "", {"src":"url/of/image.jpg"})
+        self.assertEqual(node.to_html(), '<img src="url/of/image.jpg" />', "LeafNode 'img' tag should be rendered correctly")
+
+    def test_leaf_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text", "LeafNode with no tag should return just the value")
+
+    def test_leaf_no_value(self):
+        with self.assertRaises(ValueError):
+            LeafNode("div", None).to_html()
+
+    def test_leaf_with_multiple_props(self):
+        node = LeafNode("input", "", {"type": "text", "name": "username", "placeholder": "Enter username"})
+        # Adjust expected output based on your implementation
+        # For non-self-closing tags:
+        # self.assertEqual(node.to_html(), '<input type="text" name="username" placeholder="Enter username"></input>')
+        # For self-closing tags:
+        self.assertEqual(node.to_html(), '<input type="text" name="username" placeholder="Enter username" />')
 
 if __name__ == "__main__":
     unittest.main()
